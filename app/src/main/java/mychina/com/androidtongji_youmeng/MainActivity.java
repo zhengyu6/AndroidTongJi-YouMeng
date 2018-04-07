183046b562a15de6aaaf4451b8d6a1cf5b2d9e07
zhengyu6 <3061350653@qq.com>
2018-04-07 18:38:25 +0800

    第一次提交

---
+++ app/src/main/java/mychina/com/androidtongji_youmeng/MainActivity.java
@@ -0,0 +1,26 @@
+package mychina.com.androidtongji_youmeng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+}
